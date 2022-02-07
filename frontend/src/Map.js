7a60508cb8a2a3db7e7feb6fabe33f64a60342fc
Athulya2208 <athul2219@gmail.com>
2022-02-07 09:12:45 +0530

    Changes

--- frontend/src/Map.js
+++ frontend/src/Map.js
@@ -3,29 +3,59 @@ import { useState, useEffect } from 'react';
 import ReactMapGL, { Marker, Popup } from 'react-map-gl';
 import { Room, Star } from '@material-ui/icons';
 import axios from 'axios';
-import {format} from 'timeago.js';
+import { format } from 'timeago.js';
 import './Map.css';
 
 
 function Map() {
-    const currentUser = 'athu'
-    const [pins, setPins] = useState([])
-    const [currentPlaceId, setCurrentPlaceId] = useState(null)
+    const currentUser = 'athu';
+    const [pins, setPins] = useState([]);
+    const [currentPlaceId, setCurrentPlaceId] = useState(null);
+    const [newPlace, setNewPlace] = useState(null);
+    const [title, setTitle] = useState(null);
+    const [description, setDescription] = useState(null);
+    const [rating, setRating] = useState(0);
     const [viewport, setViewport] = useState({
         width: '100vw',
         height: '100vh',
         latitude: 21.7679,
         longitude: 78.8718,
-        zoom: 5
+        zoom: 4
     });
-    const [showPopup, setShowPopup] = useState(false);
 
-    const handlePopup = () => {
-        setShowPopup(true);
+    const handleMarkerClick = (id, latitude, longitude) => {
+        setCurrentPlaceId(id);
+        //to center the selected place
+        setViewport({ ...viewport, latitude: latitude, longitude: longitude })
     }
 
-    const handleMarkerClick = (id) => {
-        setCurrentPlaceId(id);
+    const handleAddNewPlace = (e) => {
+        const [longitude, latitude] = e.lngLat;
+        setNewPlace({
+            latitude,
+            longitude
+        })
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const newPin = {
+            username: currentUser,
+            title,
+            description,
+            rating,
+            latitude: newPlace.latitude,
+            longitude: newPlace.longitude
+        }
+
+        console.log(newPin)
+        try {
+            const res = await axios.post("/pins", newPin);
+            setPins([...pins, res.data]);
+            setNewPlace(null);
+        } catch (err) {
+            console.log(err);
+        }
     }
 
     useEffect(() => {
@@ -44,8 +74,10 @@ function Map() {
         <ReactMapGL
             {...viewport}
             mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
-            onViewportChange={nextViewport => setViewport(nextViewport)}
+            onViewportChange={(viewport) => setViewport(viewport)}
             mapStyle="mapbox://styles/mapbox/streets-v9"
+            onClick={handleAddNewPlace}
+            transitionDuration="200"
         >
             {pins.map((pin) => (
                 <>
@@ -54,14 +86,14 @@ function Map() {
                         longitude={pin.longitude}
                         offsetLeft={-20}
                         offsetTop={-10}
-                        onClick={handlePopup}
                     >
-                        <Room 
-                            style={{ 
-                                    fontSize: viewport.zoom * 5, 
-                                    color: pin.username === currentUser ? 'tomato' : 'slateblue' , 
-                                    cursor: 'pointer'}}
-                            onClick={() => handleMarkerClick(pin._id)}
+                        <Room
+                            style={{
+                                fontSize: viewport.zoom * 5,
+                                color: pin.username === currentUser ? 'tomato' : 'slateblue',
+                                cursor: 'pointer'
+                            }}
+                            onClick={() => handleMarkerClick(pin._id, pin.latitude, pin.longitude)}
                         />
                     </Marker>
                     {pin._id === currentPlaceId &&
@@ -71,7 +103,7 @@ function Map() {
                             onClose={() => setCurrentPlaceId(null)}
                             anchor="bottom" >
                             <div className='card'>
-                                <label>Place</label>
+                                <label>Title</label>
                                 <h4 className='place'>{pin.title}</h4>
                                 <label>Review</label>
                                 <p className='review'>{pin.description}</p>
@@ -87,6 +119,38 @@ function Map() {
                     }
                 </>
             ))}
+            {newPlace && (
+                <Popup
+                    latitude={newPlace.latitude}
+                    longitude={newPlace.longitude}
+                    onClose={() => setNewPlace(null)}
+                    anchor="bottom"
+                >
+                    <div>
+                        <form onSubmit={handleSubmit}>
+                            <label>Title</label>
+                            <input
+                                placeholder='Enter a title'
+                                onChange={(e) => setTitle(e.target.value)}
+                            />
+                            <label>Review</label>
+                            <textarea
+                                placeholder='Say something about this place'
+                                onChange={(e) => setDescription(e.target.value)}
+                            />
+                            <label>Rating</label>
+                            <select onChange={(e) => setRating(e.target.value)}>
+                                <option value='1'>1</option>
+                                <option value='2'>2</option>
+                                <option value='3'>3</option>
+                                <option value='4'>4</option>
+                                <option value='5'>5</option>
+                            </select>
+                            <button className='add-button' type='submit'>Add Pin</button>
+                        </form>
+                    </div>
+                </Popup>
+            )}
         </ReactMapGL>
     );
 }
